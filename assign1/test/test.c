d17da640677f3a5946c226a42f107f599c799544
cocoacitrus <bowman_snowman@me.com>
2019-10-19 16:01:38 -0400

    insignificant

--- assign1/test/test.c
+++ assign1/test/test.c
@@ -5,61 +5,63 @@
 int main()
 {
 	// CASE 1: Trying to free something that isn't a pointer.
-	// int x = 0;
-	// free((int*)x);
+	int x = 0;
+	free(x);
 
 	// CASE 2a: Trying to free something that was never saved to memory.
-	// char* y = (char*)malloc(200);
-	// free(y+10);
-	// free(y);
+	char* y = (char*)malloc(200);
+	free(y+10);
+	free(y);
 
 	// Case 2b: Trying to free something that was never saved to memory.
-	// int* z = NULL;
-	// free(z);
+	int* z = NULL;
+	free(z);
 
 	// Case 3:  Trying to free something twice.
-	// char* a = (char*)malloc(100);
-	// free(a);
-	// free(a);
+	char* a = (char*)malloc(100);
+	free(a);
+	free(a);
 
 	// Case 4: Allocating something, freeing it, allocating it again, freeing it again.
-	// char* b = (char*)malloc(100);
-	// free(b);
-	// b = (char*)malloc(100);
-	// free(b);
+	char* b = (char*)malloc(100);
+	free(b);
+	b = (char*)malloc(100);
+	free(b);
 
 	// Case 5a: Allocating too much memory.
-	// char* c = (char*)malloc(4097);
+	char* c = (char*)malloc(4097);
 
 	// Case 5b: Allocating too much memory.
-	// char* d = (char*)malloc(4096-sizeof(metadata));
-	// char* e = (char*)malloc(1);
+	char* d = (char*)malloc(4096-sizeof(metadata));
+	char* e = (char*)malloc(1);
 
 	// TEST A
-	// char* f;
-	// for(int i = 0; i < 150; i++)
-	// {
-	// 	f = (char*)malloc(1);
-	// 	free(f);
-	// };
+	char* f;
+	int i = 0;
+	for(; i < 150; i++)
+	{
+		f = (char*)malloc(1);
+		free(f);
+	};
 
 	// TEST B
+/*
+	if(p != NULL)
+	{
+		printf("%c\n", *p);
+	};
+*/
 
-	// if(p != NULL)
-	// {
-	// 	printf("%c\n", *p);
-	// };
-
-	// char* q = (char*)malloc(1);
-	// if(q != NULL)
-	// {
-	// 	printf("%c\n", *q);
-	// };
+	char* q = (char*)malloc(1);
+	if(q != NULL)
+	{
+		printf("%c\n", *q);
+	};
 
 
 
-	// free(p);
-	// free(q);
+	//free(p);
+	free(q);
 
 	// printf("Testing complete.\n");
 
