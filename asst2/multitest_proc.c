55018f8d0101325e711cca83f47baac2fe6681e4
cocoacitrus <bowman_snowman@me.com>
2019-11-18 14:57:43 -0500

    search & dummy_search defined

--- asst2/multitest_proc.c
+++ asst2/multitest_proc.c
@@ -1,8 +1,22 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "multitest.h"
 
-void search(int list_size, int query) {
-  printf("Mode: Multiprocessing.\n");
-  printf("List size: %d.\n", list_size);
-  printf("Query: %d.\n", query);
+void search(int target, int** arr, int len) {
+	int i = 0;
+	while( i < len )
+	{
+		if( i == (*arr + i) )
+		{ // target found
+			exit(i);
+		}
+		i++;
+	}
+	exit(EXIT_FAILURE);
+}
+
+void dummy_search(int target, int** arr, int len, int chunksize, int* ifound)
+{
+	printf("dummy search process\n");
+	exit(EXIT_SUCCESS);
 }
