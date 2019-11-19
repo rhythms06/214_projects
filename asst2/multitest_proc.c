3e5409d0a0afdaadb627c53a7180f1fbded16cfb
cocoacitrus <bowman_snowman@me.com>
2019-11-19 13:23:48 -0500

    search() just prints given int[]

--- asst2/multitest_proc.c
+++ asst2/multitest_proc.c
@@ -1,21 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 #include "multitest.h"
 
-void search(int target, int** arr, int len) {
+void search(int target, int* arr, int len) {
 	int i = 0;
 	while( i < len )
 	{
-		if( i == (*arr + i) )
-		{ // target found
-			exit(i);
-		}
+		printf("*(arr + %d): %d\n", i, *(arr + i));
 		i++;
 	}
-	exit(EXIT_FAILURE);
+	exit(1);
 }
 
-void dummy_search(int target, int** arr, int len, int chunksize, int* ifound)
+void dummy_search(int target, int* arr, int len, int chunksize, int* ifound)
 {
 	printf("dummy search process\n");
 	exit(EXIT_SUCCESS);
