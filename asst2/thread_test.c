1f333909d2ae63957a1818524cbd047ac2a97284
cocoacitrus <bowman_snowman@me.com>
2019-11-22 18:52:25 -0500

    same as multitest_thread.c, with extra print statements

--- asst2/thread_test.c
+++ asst2/thread_test.c
@@ -22,6 +22,12 @@ static void* search_t(void* args)
 {
 	thread_info *thread = args;
 	int i, start, end;	
+	int* res = (int*)malloc(sizeof(int));
+	if( res == NULL)
+	{ 
+		fprintf(stderr, "%s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 	i = start = thread->bites * thread->thread_num;
 	end = start + thread->bites;
 	if(end >= thread->len)
@@ -29,8 +35,24 @@ static void* search_t(void* args)
 		end = thread->len;
 	}
 	printf("thread: %d\tstart: %d\tend: %d\n", thread->thread_num, start, end);
+	while(i < end)
+	{
+		if(thread->target == *(thread->arr + i))
+		{ // target found. let's get outta here!
+			i = i - start;
+			*res = i;
+			printf("\t\tTARGET FOUND.\t *res = %d\n", *res);
+			pthread_exit((void*)res);
+			//return (void *)res;
+			//return (void *)420;
+		}
+		i++;
+	}
+	return NULL;
+	/*
 	int titties = 420;
-	pthread_exit((void*)&titties);
+	pthread_exit(&titties);
+	*/
 }
 
 void dummy_search(int target, int* arr, int len, int bites, int* ifound)
@@ -61,13 +83,15 @@ void dummy_search(int target, int* arr, int len, int bites, int* ifound)
 		tnum++;
 	}
 	tnum = 0;
-	//void* res = NULL;
+	void* res = NULL;
+	/*
 	void* res = malloc(sizeof(void*));
 	if(res == NULL)
 	{ // obligatory error check
 		fprintf(stderr, "error: unable to allocate memory\n");
 		exit(EXIT_FAILURE);
 	}
+	*/
 	while(tnum < workers)
 	{
 		s = pthread_join(threads[tnum].thread_id, &res);
@@ -76,10 +100,18 @@ void dummy_search(int target, int* arr, int len, int bites, int* ifound)
 			fprintf(stderr, "error: thread %d not joined\n", tnum);
 			exit(EXIT_FAILURE);
 		}
-		printf("Joined with thread %d. res: %d\n", threads[tnum].thread_num, (int*)res);
+		if(res == NULL)
+		{
+			printf("Joined with thread %d. target NOT FOUND\n", threads[tnum].thread_num);
+		} else
+		{ // res :- index of found target in arr.
+			*ifound = *((int*)res) + (bites * threads[tnum].thread_num);
+			printf("Joined with thread %d. found %d: %d\n", threads[tnum].thread_num, target, *ifound);
+		}
 		tnum++;
 	}
 	free(res); 
 	free(threads);
-	exit(EXIT_SUCCESS);
+	return;
 }
+
