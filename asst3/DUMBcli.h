eae90589327c2319c083e2e95691367f11ffe3d4
cocoacitrus <bowman_snowman@me.com>
2019-12-03 16:39:03 -0500

    initialized headers

---
+++ asst3/DUMBcli.h
@@ -0,0 +1,24 @@
+#ifndef _DUMBcli_H
+#define _DUMBcli_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+
+/* define COMMANDS as integers (may not need)*/
+#define HELLO 0
+#define GDBYE 1
+#define CREAT 2
+#define OPNBX 3
+#define NXTMS 4
+#define PUTMG 5
+#define DELBX 6
+#define CLSBX 7
+
+
+#endif
---
+++ asst3/DUMBser.h
@@ -0,0 +1,25 @@
+#ifndef _DUMBser_H
+#define _DUMBser_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+
+/* define COMMANDS as integers (may not need)*/
+#define HELLO 0
+#define GDBYE 1
+#define CREAT 2
+#define OPNBX 3
+#define NXTMS 4
+#define PUTMG 5
+#define DELBX 6
+#define CLSBX 7
+
+
+#endif
